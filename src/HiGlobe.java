de8baf909ed5efcddc5d10d52f76d906b3e36d79
Chloe Crawford <evilbees@comcast.net>
2019-08-29 10:11:43 -0700

    Added a third println

--- src/HiGlobe.java
+++ src/HiGlobe.java
@@ -1,6 +1,7 @@
 public class HiGlobe {
     public static void main(String[] args) {
         System.out.println("Hi Globe!");
-        System.out.println("H e l l o   L i v a b l e   S p h e r e")
+        System.out.println("H e l l o   L i v a b l e   S p h e r e");
+        System.out.println("I n h a l e   t h e   L i f e   G a s");
     }
 }
